a8491170568b8f3dd65d05664cc7f322502f9af5
Josip S <35076900+jskako@users.noreply.github.com>
2022-09-23 00:27:11 +0200

    Update utils.py

--- com.jits/utils.py
+++ com.jits/utils.py
@@ -7,10 +7,10 @@ def get_folder():
     return askdirectory(title='Select Folder')
 
 
-def get_movies_from_folder(directory=get_folder()):
+def get_movies_from_folder(directory=get_folder(), extensions_list=movie_extensions):
     file_list = []
     for (dir_path, dir_names, file_names) in os.walk(directory):
         for file in file_names:
-            if file.endswith(tuple(movie_extensions)):
+            if file.endswith(tuple(extensions_list)):
                 file_list.append(file)
     return file_list
