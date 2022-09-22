9ad31bc2b7523a920bdda9cec322f8dd6a821a0d
Josip S <35076900+jskako@users.noreply.github.com>
2022-09-23 00:31:38 +0200

    Update main.py

--- com.jits/main.py
+++ com.jits/main.py
@@ -1,9 +1,19 @@
 from utils import *
-
+import argparse
 
 def main():
     list_of_files = get_movies_from_folder()
     print(list_of_files)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    #TODO: should set required=True
+    parser.add_argument('-d', '--directory', type=str, help="Directory to search video movies/series in")
+    parser.add_argument('-r', '--recursive', action="store_true", help="wheter to recursively search all subfolders")
+    args = parser.parse_args()
+    print(args.directory)
+    print(args.recursive)
 
 
 if __name__ == '__main__':
